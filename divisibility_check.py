596e57bd1a57a3d63e65042d490a3cde74d8cf7b
21ccronk <21ccronk@athenian.org>
2020-09-11 16:59:45 +0000

    Finished

--- divisibility_check.py
+++ divisibility_check.py
@@ -3,14 +3,12 @@
 # I'm havng trouble getting it to recognize whether it's divisible or not and then printing acordingly.
 # I tried using for loops before but that wasn't working
 
+for x in range(2,51):
 
-x = input("Enter a number between 2 and 50:")
-
-x % 3 == 0
-print("x is divisible by 3.")
-
-x % 3 != 0
-print("x is not divisible by 3.")
+    if x % 3 == 0:
+        print(f"{x} is divisible by 3.")
+    else:
+        print(f"{x} is not divisible by 3.")
 
     
 
